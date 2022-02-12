d79a8695babb9a8460438879b30263a9126aa1ba
chanwool <anfto10@gmail.com>
2022-02-12 23:28:41 +0900

    vue.js 공부

---
+++ vue.js/study/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  name: 'Header',
+  components: {
+    Header,
+  }
+}
+</script>
+
